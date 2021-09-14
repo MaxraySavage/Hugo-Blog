dd6e56a15aad06a5c4482b566f34ce0f39018b59
Maxray Savage <MaxraySavage@users.noreply.github.com>
2021-09-14 14:44:54 -0400

    fix deploy script so that theme changes dont break the site

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,13 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+# Erase previous build.
+cd public
+git rm -r .
+cd ..
+
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t PaperMod # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
